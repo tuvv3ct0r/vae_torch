8368dcd1a25bd873c2fb7ab2b055fcb1f6109e80
Collins Mwaura <cmwambui@proton.me>
2025-05-24 18:08:49 +0300

    Add CelebA dataset and data module for VAE training

---
+++ dataset.py
@@ -0,0 +1,99 @@
+from pathlib import Path
+from typing import List, Optional, Sequence, Union
+
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+from torchvision import transforms
+from torchvision.datasets import CelebA
+
+from pytorch_lightning import LightningDataModule
+
+
+class CelebADataset(Dataset):
+    def __init__(self, root, split="train", download=False, transform=None):
+        self.celeba = CelebA(root, split=split, download=download, transform=transform)
+
+
+    def __getitem__(self, index):
+        return self.celeba[index]
+
+    def __len__(self):
+        return len(self.celeba)
+    
+
+
+class CelebADataModule(LightningDataModule):
+    def __init__(
+        self,
+        data_path: str,
+        train_batch_size: int = 8,
+        val_batch_size: int = 8,
+        patch_size: Union[int, Sequence[int]] = (256, 256),
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ):
+        super().__init__()
+
+        self.data_dir = data_path
+        self.train_batch_size = train_batch_size
+        self.val_batch_size = val_batch_size
+        self.patch_size = patch_size
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory
+
+    def setup(self, stage=Optional[str]) -> None:
+        train_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                              transforms.CenterCrop(148),
+                                              transforms.Resize(self.patch_size),
+                                              transforms.ToTensor(),
+                                              ])
+        
+        val_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                            transforms.CenterCrop(148),
+                                            transforms.Resize(self.patch_size),
+                                            transforms.ToTensor(),
+                                            ])
+        
+        self.train_dataset = CelebADataset(
+            self.data_dir,
+            split='train',
+            transform=train_transforms,
+            download=False,
+        )
+        
+        # Replace CelebA with your dataset
+        self.val_dataset = CelebADataset(
+            self.data_dir,
+            split='test',
+            transform=val_transforms,
+            download=False,
+        )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=True,
+            pin_memory=self.pin_memory
+            )
+
+    def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.val_batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            pin_memory=self.pin_memory,
+        )
+    
+    def test_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(
+            self.val_dataset,
+            batch_size=144,
+            num_workers=self.num_workers,
+            shuffle=True,
+            pin_memory=self.pin_memory,
+        )
+     
